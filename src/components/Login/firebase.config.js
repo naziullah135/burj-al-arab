27458238e8440621427cb8442dd3e09cefb8f0b2
Naziullah Shawn <naziullahshawn135@gmail.com>
2021-03-28 23:03:34 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDgXZH0Ztd3NMJ509koGWHYpQ_93p6tcyk",
+  authDomain: "burj-al-arab-1cabb.firebaseapp.com",
+  projectId: "burj-al-arab-1cabb",
+  storageBucket: "burj-al-arab-1cabb.appspot.com",
+  messagingSenderId: "930770272522",
+  appId: "1:930770272522:web:9d4a54e9ade2f7c2e8baf3"
+};
+  export default firebaseConfig;
